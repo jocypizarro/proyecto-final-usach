f310bae187ce335e45c42e5943c5f83f22d32ed1
anemonadja <nadja.villarroel.n@gmail.com>
2026-07-30 23:44:24 -0400

    cambios termporales en globals y layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Inter } from 'next/font/google';
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const interFont = Inter({
+  variable: "--font-inter",
+  subsets: ['latin'],
+  weight: "400",
 });
 
 export const metadata: Metadata = {
@@ -26,8 +22,8 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      lang="es"
+      className={`m-6 ${interFont.className} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         <Navbar />
