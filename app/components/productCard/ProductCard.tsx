7c5df245041ea77f75f74c6fffbd9e8d87730f36
anemonadja <nadja.villarroel.n@gmail.com>
2026-08-01 07:48:47 -0400

    creación de página tienda, cards y uso de Fake Store API

---
+++ app/components/productCard/ProductCard.tsx
@@ -0,0 +1,41 @@
+import styles from "./ProductCard.module.css";
+ 
+interface ProductCardProps {
+  id: number;
+  title: string;
+  price: number;
+  image: string;
+  isFavorite: boolean;
+  onToggleFavorite: (id: number) => void;
+}
+ 
+export default function ProductCard({
+  id,
+  title,
+  price,
+  image,
+  isFavorite,
+  onToggleFavorite,
+}: ProductCardProps) {
+  return (
+    <div className={styles.card}>
+      <button
+        className={styles.favoriteButton}
+        onClick={() => onToggleFavorite(id)}
+        aria-label={isFavorite ? "Quitar de favoritos" : "Agregar a favoritos"}
+      >
+        {isFavorite ? "♥" : "♡"}
+      </button>
+ 
+      <div className={styles.imageWrapper}>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src={image} alt={title} className={styles.image} />
+      </div>
+ 
+      <h3 className={styles.title}>{title}</h3>
+      <p className={styles.price}>${price.toFixed(2)}</p>
+ 
+      <button className={styles.buyButton}>Comprar</button>
+    </div>
+  );
+}
