60b2ea54cf980c8cbc332844b745bc9e4add77da
jocypizarro <jocelinepizarrodiaz@gmail.com>
2026-08-02 14:45:36 -0400

    feature: sección testimonio bajo el slider

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Slider from "./components/Slider";
+import Testimonios from "./components/Testimonios";
 import Bienvenida from "./components/Bienvenida";
 
 export default function Home() {
   return (
     <>
       <Slider />
+      <Testimonios />
       <Bienvenida />
     </>
   );
