a12df5d8834fddf6cbbe134241d6212bd40fbb12
jocypizarro <jocelinepizarrodiaz@gmail.com>
2026-08-02 16:33:25 -0400

    fix: eliminar destacados

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import Slider from "./components/Slider";
 import Bienvenida from "./components/Bienvenida";
-import Destacados from "./components/Destacados/Destacados";
 
 export default function Home() {
   return (
     <>
       <Slider />
-      <Destacados />
       <Bienvenida />
     </>
   );
